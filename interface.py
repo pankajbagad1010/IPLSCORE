df52ae4fff1c3a1daf164557ce2f07daf0bcc86d
git <pankajbagad1010@gmail.com>
2023-07-11 12:23:56 +0530

    new files commit

--- interface.py
+++ interface.py
@@ -1,109 +1,53 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 2,
-   "id": "c558ade3",
-   "metadata": {},
-   "outputs": [
-    {
-     "ename": "ModuleNotFoundError",
-     "evalue": "No module named 'utils'",
-     "output_type": "error",
-     "traceback": [
-      "\u001b[1;31m---------------------------------------------------------------------------\u001b[0m",
-      "\u001b[1;31mModuleNotFoundError\u001b[0m                       Traceback (most recent call last)",
-      "\u001b[1;32m~\\AppData\\Local\\Temp\\ipykernel_12828\\1742174161.py\u001b[0m in \u001b[0;36m<module>\u001b[1;34m\u001b[0m\n\u001b[0;32m      1\u001b[0m \u001b[1;32mfrom\u001b[0m \u001b[0mflask\u001b[0m \u001b[1;32mimport\u001b[0m \u001b[0mFlask\u001b[0m\u001b[1;33m,\u001b[0m \u001b[0mrequest\u001b[0m\u001b[1;33m,\u001b[0m \u001b[0mjsonify\u001b[0m\u001b[1;33m,\u001b[0m \u001b[0mrender_template\u001b[0m\u001b[1;33m,\u001b[0m \u001b[0mredirect\u001b[0m\u001b[1;33m,\u001b[0m \u001b[0murl_for\u001b[0m\u001b[1;33m\u001b[0m\u001b[1;33m\u001b[0m\u001b[0m\n\u001b[1;32m----> 2\u001b[1;33m \u001b[1;32mfrom\u001b[0m \u001b[0mutils\u001b[0m \u001b[1;32mimport\u001b[0m \u001b[0miplscore\u001b[0m\u001b[1;33m\u001b[0m\u001b[1;33m\u001b[0m\u001b[0m\n\u001b[0m\u001b[0;32m      3\u001b[0m \u001b[1;32mimport\u001b[0m \u001b[0mconfig\u001b[0m\u001b[1;33m\u001b[0m\u001b[1;33m\u001b[0m\u001b[0m\n\u001b[0;32m      4\u001b[0m \u001b[0mapp\u001b[0m \u001b[1;33m=\u001b[0m \u001b[0mFlask\u001b[0m\u001b[1;33m(\u001b[0m\u001b[0m__name__\u001b[0m\u001b[1;33m)\u001b[0m\u001b[1;33m\u001b[0m\u001b[1;33m\u001b[0m\u001b[0m\n\u001b[0;32m      5\u001b[0m \u001b[1;33m\u001b[0m\u001b[0m\n",
-      "\u001b[1;31mModuleNotFoundError\u001b[0m: No module named 'utils'"
-     ]
-    }
-   ],
-   "source": [
-    "from flask import Flask, request, jsonify, render_template, redirect, url_for\n",
-    "from utils import iplscore\n",
-    "import config\n",
-    "import traceback\n",
-    "app = Flask(__name__)\n",
-    "\n",
-    "@app.route('/ipsscore')\n",
-    "def home1():\n",
-    "    return render_template('iplscore.html')\n",
-    "\n",
-    "@app.route('/ipl', methods = ['GET', 'POST'])\n",
-    "def predict_score():\n",
-    "    try:\n",
-    "        if request.method == 'GET':\n",
-    "            print(\"+\"*50)\n",
-    "            data = request.args.get\n",
-    "            print(\"Data :\",data)\n",
-    "            batting_team = data('batting_team')\n",
-    "            bowling_team = data('bowling_team')\n",
-    "            overs = int(data('overs'))\n",
-    "            runs = int(data('runs'))\n",
-    "            wickets = int(data('wickets'))\n",
-    "            runs_in_prev_5 = int(data('runs_in_prev_5'))\n",
-    "            wickets_in_prev_5 = int(data('wickets_in_prev_5'))\n",
-    "            \n",
-    "\n",
-    "            Obj = iplscore(batting_team, bowling_team, overs, runs,wickets, runs_in_prev_5,wickets_in_prev_5)\n",
-    "            pred_score = Obj.get_predicted_score()\n",
-    "            \n",
-    "            return render_template('iplscore.html', prediction = pred_score)\n",
-    "       \n",
-    "        elif request.method == 'POST':\n",
-    "            print(\"*\"*40)\n",
-    "            data = request.form.get\n",
-    "            print(\"Data :\",data)\n",
-    "            batting_team = data('batting_team')\n",
-    "            bowling_team = data('bowling_team')\n",
-    "            overs = int(data('overs'))\n",
-    "            runs = int(data('runs'))\n",
-    "            wickets = int(data('wickets'))\n",
-    "            runs_in_prev_5 = int(data('runs_in_prev_5'))\n",
-    "            wickets_in_prev_5 = int(data('wickets_in_prev_5'))\n",
-    "            \n",
-    "\n",
-    "            Obj = iplscore(batting_team, bowling_team, overs, runs,wickets, runs_in_prev_5,wickets_in_prev_5)\n",
-    "            pred_score = Obj.get_predicted_score()\n",
-    "            \n",
-    "            return render_template('iplscore.html', prediction = pred_score)\n",
-    "\n",
-    "    except:\n",
-    "        print(traceback.print_exc())\n",
-    "        return redirect(url_for('ipsscore'))\n",
-    "\n",
-    "if __name__ == \"__main__\":\n",
-    "    app.run(host='0.0.0.0', port=config.PORT_NUMBER)\n"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "869d13ba",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.9.13"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+from flask import Flask, request, jsonify, render_template, redirect, url_for
+from utils import iplscore
+import config
+import traceback
+app = Flask(__name__)
+
+@app.route('/ipsscore')
+def home1():
+  return render_template('iplscore.html')
+
+@app.route('/ipl', methods = ['GET', 'POST'])
+def predict_score():
+    try:
+        if request.method == 'GET':
+            print("+"*50)
+            data = request.args.get
+            print("Data :",data)
+            batting_team = data('batting_team')
+            bowling_team = data('bowling_team')
+            overs = int(data('overs'))
+            runs = int(data('runs'))
+            wickets = int(data('wickets'))
+            runs_in_prev_5 = int(data('runs_in_prev_5'))
+            wickets_in_prev_5 = int(data('wickets_in_prev_5'))
+
+            Obj = iplscore(batting_team, bowling_team, overs, runs,wickets, runs_in_prev_5,wickets_in_prev_5)
+            pred_score = Obj.get_predicted_score()
+            return render_template('iplscore.html', prediction = pred_score)
+
+        elif request.method == 'POST':
+                print("*"*40)
+                data = request.form.get
+                print("Data :",data)
+                batting_team = data('batting_team')
+                bowling_team = data('bowling_team')
+                overs = int(data('overs'))
+                runs = int(data('runs'))
+                wickets = int(data('wickets'))
+                runs_in_prev_5 = int(data('runs_in_prev_5'))
+                wickets_in_prev_5 = int(data('wickets_in_prev_5'))
+
+
+                Obj = iplscore(batting_team, bowling_team, overs, runs,wickets, runs_in_prev_5,wickets_in_prev_5)
+                pred_score = Obj.get_predicted_score()
+                return render_template('iplscore.html', prediction = pred_score)
+
+    except:
+    
+            print(traceback.print_exc())
+            return redirect(url_for('ipsscore'))
+
+    if __name__ == "__main__":
+        app.run(host='0.0.0.0', port=config.PORT_NUMBER)
--- utils.py
+++ utils.py
@@ -1,92 +1,35 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 3,
-   "id": "65560963",
-   "metadata": {},
-   "outputs": [
-    {
-     "ename": "ModuleNotFoundError",
-     "evalue": "No module named 'config'",
-     "output_type": "error",
-     "traceback": [
-      "\u001b[1;31m---------------------------------------------------------------------------\u001b[0m",
-      "\u001b[1;31mModuleNotFoundError\u001b[0m                       Traceback (most recent call last)",
-      "\u001b[1;32m~\\AppData\\Local\\Temp\\ipykernel_13776\\1439589444.py\u001b[0m in \u001b[0;36m<module>\u001b[1;34m\u001b[0m\n\u001b[0;32m      2\u001b[0m \u001b[1;32mimport\u001b[0m \u001b[0mjson\u001b[0m\u001b[1;33m\u001b[0m\u001b[1;33m\u001b[0m\u001b[0m\n\u001b[0;32m      3\u001b[0m \u001b[1;32mimport\u001b[0m \u001b[0mnumpy\u001b[0m \u001b[1;32mas\u001b[0m \u001b[0mnp\u001b[0m\u001b[1;33m\u001b[0m\u001b[1;33m\u001b[0m\u001b[0m\n\u001b[1;32m----> 4\u001b[1;33m \u001b[1;32mimport\u001b[0m \u001b[0mconfig\u001b[0m\u001b[1;33m\u001b[0m\u001b[1;33m\u001b[0m\u001b[0m\n\u001b[0m\u001b[0;32m      5\u001b[0m \u001b[1;33m\u001b[0m\u001b[0m\n\u001b[0;32m      6\u001b[0m \u001b[1;32mclass\u001b[0m \u001b[0miplscore\u001b[0m\u001b[1;33m(\u001b[0m\u001b[1;33m)\u001b[0m\u001b[1;33m:\u001b[0m\u001b[1;33m\u001b[0m\u001b[1;33m\u001b[0m\u001b[0m\n",
-      "\u001b[1;31mModuleNotFoundError\u001b[0m: No module named 'config'"
-     ]
-    }
-   ],
-   "source": [
-    "import pickle\n",
-    "import json\n",
-    "import numpy as np\n",
-    "import config\n",
-    "\n",
-    "class iplscore():\n",
-    "    def __init__(self, batting_team, bowling_team, overs, runs,wickets, runs_in_prev_5,wickets_in_prev_5):\n",
-    "        print(\"****** INIT Function *********\")\n",
-    "        self.batting_team = batting_team\n",
-    "        self.bowling_team = bowling_team\n",
-    "        self.overs = overs\n",
-    "        self.runs = runs\n",
-    "        self.wickets = wickets\n",
-    "        self.runs_in_prev_5 = runs_in_prev_5\n",
-    "        self.wickets_in_prev_5 = wickets_in_prev_5\n",
-    "        \n",
-    "    def __load_saved_data(self):\n",
-    "        with open(config.MODEL_FILE_PATH,'rb') as f:\n",
-    "            self.model = pickle.load(f)\n",
-    "            \n",
-    "        with open(config.JSON_FILE_PATH,'r') as f:\n",
-    "            self.json_data = json.load(f)\n",
-    "    \n",
-    "    def get_predicted_score(self):\n",
-    "        self.__load_saved_data()\n",
-    "        \n",
-    "        test_array = np.zeros([1,self.model.n_features_in_])\n",
-    "        test_array[0,0] = self.batting_team\n",
-    "        test_array[0,1] = self.bowling_team\n",
-    "        test_array[0,2] = self.overs\n",
-    "        test_array[0,3] = self.runs\n",
-    "        test_array[0,4] = self.wickets\n",
-    "        test_array[0,5] = self.runs_in_prev_5\n",
-    "        test_array[0,6] = self.wickets_in_prev_5\n",
-    "\n",
-    "\n",
-    "        predicted_score = np.around(self.model.predict(test_array)[0],3)\n",
-    "        return predicted_score"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "cf170fd3",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.9.13"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+import pickle
+import json
+import numpy as np
+import config
+class iplscore():
+    def __init__(self, batting_team, bowling_team, overs, runs,wickets, runs_in_prev_5,wickets_in_prev_5):
+        print("****** INIT Function *********")
+        self.batting_team = batting_team
+        self.bowling_team = bowling_team
+        self.overs = overs
+        self.runs = runs
+        self.wickets = wickets
+        self.runs_in_prev_5 = runs_in_prev_5
+        self.wickets_in_prev_5 = wickets_in_prev_5
+
+def __load_saved_data(self):
+        with open(config.MODEL_FILE_PATH,'rb') as f:
+            self.model = pickle.load(f)
+
+        with open(config.JSON_FILE_PATH,'r') as f:
+            self.json_data = json.load(f)
+
+def get_predicted_score(self):
+    self.__load_saved_data()
+    test_array = np.zeros([1,self.model.n_features_in_])
+    test_array[0,0] = self.batting_team
+    test_array[0,1] = self.bowling_team
+    test_array[0,2] = self.overs
+    test_array[0,3] = self.runs
+    test_array[0,4] = self.wickets
+    test_array[0,5] = self.runs_in_prev_5
+    test_array[0,6] = self.wickets_in_prev_5
+
+    predicted_score = np.around(self.model.predict(test_array)[0],3)
+    return predicted_score
